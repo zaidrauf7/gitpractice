e980e81698a60d39b97b890b03240e780336e0a8
zaidrauf7 <iamzaidrauf@gmail.com>
2026-01-29 16:11:10 +0500

    changes

---
+++ src/pages/Help.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Help = () => {
+  return (
+    <div>
+        <h1>hello this is awesome</h1>
+        <h1>hello this is awesome</h1>
+        <h1>hello this is awesome</h1>
+        <h1>hello this is awesome</h1>
+        <h1>hello this is awesome</h1>
+        <h1>hello this is awesome</h1>
+    </div>
+  )
+}
+
+export default Help
